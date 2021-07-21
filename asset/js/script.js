2617e57867f98b297ee0ef2c630a6bb5f9e465cf
Steph-en <appahstephen9@gmail.com>
2021-07-21 10:56:18 +0000

    made a few tweaks

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,6 +1,6 @@
 const sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong']
 
-sounds.forEach(sounds => {
+sounds.forEach(sound => {
     const btn = document.createElement('button')
     btn.classList.add('btn')
 
